e2dac8f33e309fcc45cda69c885e45c0fada7a2d
steph <steph@STEPHAN-MAIN>
2016-12-20 16:27:19 +0100

    Android Target prepared

--- src/GameLibrary.Android/MainActivity.cs
+++ src/GameLibrary.Android/MainActivity.cs
@@ -4,9 +4,16 @@ using Android.OS;
 using Xamarin.Forms.Platform.Android;
 using GameLibrary.Client.UI;
 
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
 namespace GameLibrary.Android
 {
-    [Activity(Label = "GameLibrary.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "GameLibrary.Android", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
