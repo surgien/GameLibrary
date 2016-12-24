c9c28ff8acab1cd952a4cadaf8fc433374e87b66
surgien <stephan.urgien@outlook.de>
2016-12-24 21:15:03 +0100

    Test

--- src/GameLibrary.Client.Core/ViewModel/MainNavigationViewModel.cs
+++ src/GameLibrary.Client.Core/ViewModel/MainNavigationViewModel.cs
@@ -1,9 +1,6 @@
 ﻿using FormsToolkit;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
